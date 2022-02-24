9bb29f67d22dadf5ef7aaf84e52999a8deaa3bd7
NickMarcha <exstracrazy@gmail.com>
2022-02-24 15:18:49 +0100

    backend foler /w spring boot

---
+++ backend/src/test/java/no/hvl/dat251/v22/SikkerKommunikasjon/SikkerKommunikasjonApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat251.v22.SikkerKommunikasjon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikkerKommunikasjonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
