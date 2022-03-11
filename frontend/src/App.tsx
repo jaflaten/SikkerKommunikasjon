2c2fc84565dddf7b2fbb54e2f6550f01d6c429e4
NickMarcha <exstracrazy@gmail.com>
2022-03-11 16:08:44 +0100

    test fetch from frontend to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  fetch("http://localhost:1337/api", { mode: "no-cors" })
+    .then((res) => res)
+    .then((result) => {
+      console.log(result);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
