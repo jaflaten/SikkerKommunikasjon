bc60cd5b9e9d5e3769075699f5aba62b7b39b449
jaflaten <ja.flaten91@gmail.com>
2022-03-14 09:48:01 +0100

    pre-demo test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{ color: "#fba161" }}>Sikker Kommunikasjon</h1>
+        <h1 style={{ color: "#fba161" }}>pre-demo test Sikker Kommunikasjon</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
