9bc9739abe8e172ca7dbcd2c81dae2d9cabfe25d
Einar Haaland <Einar.Haaland@student.uib.no>
2022-03-20 19:42:19 +0100

    Add radio buttons

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -20,79 +20,108 @@ const Form = () => {
     return (
         <div>
             <form>
-                <h2>Hvem Sender Inn?</h2>
-                <label>
-                    Personnummer
-                    <input 
-                        type="text"  
-                        name="ssn" 
-                        value={formData.ssn}
-                        onChange={(e) => setFormData({...formData, ssn: e.target.value})}
-                    />
-                </label>
-                <label>
-                    Navn
-                    <input 
-                        type="text"  
-                        name="name" 
-                        value={formData.name}
-                        onChange={(e) => setFormData({...formData, name: e.target.value})}
-                    />
-                </label>
-                <label>
-                    Epost
-                    <input 
-                        type="text"  
-                        name="email" 
-                        value={formData.email}
-                        onChange={(e) => setFormData({...formData, email: e.target.value})}
-                    />
-                </label>
+                <div>
+                    <h2>Hvem Sender Inn?</h2>
+                    <label>
+                        Personnummer
+                        <input 
+                            required 
+                            type="text"  
+                            name="ssn" 
+                            value={formData.ssn}
+                            onChange={(e) => setFormData({...formData, ssn: e.target.value})}
+                        />
+                    </label>
+                    <label>
+                        Navn
+                        <input 
+                            required
+                            type="text"  
+                            name="name" 
+                            value={formData.name}
+                            onChange={(e) => setFormData({...formData, name: e.target.value})}
+                        />
+                    </label>
+                    <label>
+                        Epost
+                        <input 
+                            required
+                            type="email"  
+                            name="email" 
+                            value={formData.email}
+                            onChange={(e) => setFormData({...formData, email: e.target.value})}
+                        />
+                    </label>
+                </div>
 
-                <h2>Hvem er Mottaker?</h2>
-                <label>
-                    Mottaker
-                    <input 
-                        type="text"  
-                        name="receiver" 
-                        value={formData.receiver}
-                        onChange={(e) => setFormData({...formData, receiver: e.target.value})}
-                    />
-                </label>
+                <div>
+                    <h2>Hvem er Mottaker?</h2>
+                    <label>
+                        Mottaker
+                        <input 
+                            required
+                            type="text"  
+                            name="receiver" 
+                            value={formData.receiver}
+                            onChange={(e) => setFormData({...formData, receiver: e.target.value})}
+                        />
+                    </label>
+                </div>
 
-                <h2>Hva skal sendes?</h2>
-                <label>
-                    Tittel
-                    <input 
-                        type="text"  
-                        name="title" 
-                        value={formData.title}
-                        onChange={(e) => setFormData({...formData, title: e.target.value})}
-                    />
-                </label>
-                <label>
-                    Kommentar
-                    <input 
-                        type="text"  
-                        name="message" 
-                        value={formData.message}
-                        onChange={(e) => setFormData({...formData, message: e.target.value})}
-                    />
-                </label>
-                <label>
-                    Henvendelsen inneholder sensitive eller følsomme opplysninger
-                    <input 
-                        type="checkbox"
-                        name="isSensitive"
-                        onChange={(e) => setFormData({...formData, isSensitive: e.target.checked})}
-                    />
-                </label>
-                <label>
-                    <input
-                        type="file"
-                        onChange={(e) => setFormData({...formData, selectedFile: e.target.files[0]})} //only accepts 1 fileupload
-                    />
-                </label>
+                <div>
+                    <h2>Hva skal sendes?</h2>
+                    <label>
+                        Tittel
+                        <input 
+                            required
+                            type="text"  
+                            name="title" 
+                            value={formData.title}
+                            onChange={(e) => setFormData({...formData, title: e.target.value})}
+                        />
+                    </label>
+                    <label>
+                        Kommentar
+                        <textarea 
+                            required
+                            name="message" 
+                            value={formData.message}
+                            onChange={(e) => setFormData({...formData, message: e.target.value})}
+                        />
+                    </label>
+                </div>
+
+                <div>
+                    <h2>Inneholder henvendelsen sensitive eller følsomme opplysninger?</h2>
+                    <label>
+                        Ja:
+                        <input 
+                            checked={formData.isSensitive}
+                            type="radio"
+                            name="isSensitive"
+                            onChange={(e) => setFormData({...formData, isSensitive: e.target.checked})}
+                        />
+                    </label>
+                    <label>
+                        Nei:
+                        <input 
+                            checked={!formData.isSensitive}
+                            type="radio"
+                            name="isSensitive"
+                            onChange={(e) => setFormData({...formData, isSensitive: !(e.target.checked)})}
+                        />
+                    </label>
+                </div>
+
+                <div>
+                    <label>
+                        <input
+                            type="file"
+                            onChange={(e) => setFormData({...formData, selectedFile: e.target.files[0]})} //only accepts 1 fileupload
+                        />
+                    </label>
+                </div>
+
                 <button type="button" onClick={handleSubmit}>Send Sikker Melding</button>
             </form>
         </div>
