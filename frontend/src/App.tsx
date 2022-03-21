96a3dc5b3fe28a7b7a4c6791fe8acbce509dea79
h578011 <nicolasmmjoes@gmail.com>
2022-03-21 13:09:53 +0100

    Cleaned up test fetch a bit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,25 @@ import "./App.css";
 import Form from './components/Form.tsx'
 
 function App() {
-  console.log(process.env)
-  console.log(process.env.REACT_APP_API_URL)
-  
-  fetch("/api/v1/capabilities/991825827")
+  console.log("Environment:");
+  console.log(process.env);
+  console.log("Test Fetching:");
+  let capabilitiesURL = process.env.REACT_APP_API_URL + "/v1/capabilities/991825827";
+  console.log(capabilitiesURL);
+
+  fetch(capabilitiesURL)
     .then((res) => res)
-    .then((result) => {
-      result.json().then((res) => console.log(res));
-    })
-    .catch(console.log);
+    .then((result) =>
+      result.json()
+    )
+    .then((res) => console.log(res))
+    .catch((e) => {
+      if(e.message === "Failed to fetch"){
+        console.log("Failed to fetch endpoint, are you sure service is running?")
+      }else {
+      console.log(e)
+      }
+    });
 
   return (
     <Form />
