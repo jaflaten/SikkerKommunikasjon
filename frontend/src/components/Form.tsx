0dbf96dfd8e2cb121c7a6e107d16777a6d2605d2
h578011 <nicolasmmjoes@gmail.com>
2022-03-21 14:11:39 +0100

    reverted to function structure

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,143 +1,138 @@
 import React from 'react';
 import { useState } from "react";
 
-class Form extends React.Component<{}, { ssn: string, name: string, email: string, receiver: string, title: string, message: string, isSensitive: boolean, selectedFile: File }> {
+const Form = () => {
+    const [formData, setFormData] = useState({
+        ssn: "",
+        name: "",
+        email: "",
+        receiver: "",
+        title: "",
+        message: "",
+        isSensitive: false,
+        selectedFile: null
+      })
 
-    constructor(props) {
-        super(props);
-        this.state =
-        {
-            ssn: "",
-            name: "",
-            email: "",
-            receiver: "",
-            title: "",
-            message: "",
-            isSensitive: false,
-            selectedFile: null
-        };
-    }
-    private handleSubmit() {
+    const handleSubmit = () => {
+        //implement handleSubmit
         console.log("Submit form handle: ");
         var form = (document.getElementById("form") as HTMLFormElement);
         var isFormValid = form.checkValidity();
         console.log("Is form valid? " + isFormValid);
-        
+
     }
 
-    render() {
-        return (
-            <div>
-                <form id="form">
-                    <div>
-                        <h2>Hvem Sender Inn?</h2>
-                        <label>
-                            Personnummer
-                            <input
-                                required
-                                pattern='^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])\d{7}$'
-                                type="text"
-                                name="ssn"
-                                value={this.state.ssn}
-                                onChange={(e) => this.setState({ ssn: e.target.value })}
-                            />
-                        </label>
-                        <label>
-                            Navn
-                            <input
-                                required
-                                type="text"
-                                name="name"
-                                value={this.state.name}
-                                onChange={(e) => this.setState({ name: e.target.value })}
-                            />
-                        </label>
-                        <label>
-                            Epost
-                            <input
-                                required
-                                type="email"
-                                name="email"
-                                value={this.state.email}
-                                onChange={(e) => this.setState({ email: e.target.value })}
-                            />
-                        </label>
-                    </div>
+    return (
+        <div>
+            <form>
+                <div>
+                    <h2>Hvem Sender Inn?</h2>
+                    <label>
+                        Personnummer
+                        <input 
+                            required 
+                            pattern='^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])\d{7}$'
+                            type="text"  
+                            name="ssn" 
+                            value={formData.ssn}
+                            onChange={(e) => setFormData({...formData, ssn: e.target.value})}
+                        />
+                    </label>
+                    <label>
+                        Navn
+                        <input 
+                            required
+                            type="text"  
+                            name="name" 
+                            value={formData.name}
+                            onChange={(e) => setFormData({...formData, name: e.target.value})}
+                        />
+                    </label>
+                    <label>
+                        Epost
+                        <input 
+                            required
+                            type="email"  
+                            name="email" 
+                            value={formData.email}
+                            onChange={(e) => setFormData({...formData, email: e.target.value})}
+                        />
+                    </label>
+                </div>
 
-                    <div>
-                        <h2>Hvem er Mottaker?</h2>
-                        <label>
-                            Mottaker
-                            <input
-                                required
-                                type="text"
-                                name="receiver"
-                                pattern='^([0-9]{4}:)?([0-9]{9})$'
-                                value={this.state.receiver}
-                                onChange={(e) => this.setState({ receiver: e.target.value })}
-                            />
-                        </label>
-                    </div>
+                <div>
+                    <h2>Hvem er Mottaker?</h2>
+                    <label>
+                        Mottaker
+                        <input 
+                            required
+                            type="text"  
+                            name="receiver" 
+                            pattern='^([0-9]{4}:)?([0-9]{9})$'
+                            value={formData.receiver}
+                            onChange={(e) => setFormData({...formData, receiver: e.target.value})}
+                        />
+                    </label>
+                </div>
 
-                    <div>
-                        <h2>Hva skal sendes?</h2>
-                        <label>
-                            Tittel
-                            <input
-                                required
-                                type="text"
-                                name="title"
-                                value={this.state.title}
-                                onChange={(e) => this.setState({ title: e.target.value })}
-                            />
-                        </label>
-                        <label>
-                            Kommentar
-                            <textarea
-                                required
-                                name="message"
-                                value={this.state.message}
-                                onChange={(e) => this.setState({ message: e.target.value })}
-                            />
-                        </label>
-                    </div>
+                <div>
+                    <h2>Hva skal sendes?</h2>
+                    <label>
+                        Tittel
+                        <input 
+                            required
+                            type="text"  
+                            name="title" 
+                            value={formData.title}
+                            onChange={(e) => setFormData({...formData, title: e.target.value})}
+                        />
+                    </label>
+                    <label>
+                        Kommentar
+                        <textarea 
+                            required
+                            name="message" 
+                            value={formData.message}
+                            onChange={(e) => setFormData({...formData, message: e.target.value})}
+                        />
+                    </label>
+                </div>
 
-                    <div>
-                        <h2>Inneholder henvendelsen sensitive eller følsomme opplysninger?</h2>
-                        <label>
-                            Ja:
-                            <input
-                                checked={this.state.isSensitive}
-                                type="radio"
-                                name="isSensitive"
-                                onChange={(e) => this.setState({ isSensitive: e.target.checked })}
-                            />
-                        </label>
-                        <label>
-                            Nei:
-                            <input
-                                checked={!this.state.isSensitive}
-                                type="radio"
-                                name="isSensitive"
-                                onChange={(e) => this.setState({ isSensitive: !(e.target.checked) })}
-                            />
-                        </label>
-                    </div>
+                <div>
+                    <h2>Inneholder henvendelsen sensitive eller følsomme opplysninger?</h2>
+                    <label>
+                        Ja:
+                        <input 
+                            checked={formData.isSensitive}
+                            type="radio"
+                            name="isSensitive"
+                            onChange={(e) => setFormData({...formData, isSensitive: e.target.checked})}
+                        />
+                    </label>
+                    <label>
+                        Nei:
+                        <input 
+                            checked={!formData.isSensitive}
+                            type="radio"
+                            name="isSensitive"
+                            onChange={(e) => setFormData({...formData, isSensitive: !(e.target.checked)})}
+                        />
+                    </label>
+                </div>
 
-                    <div>
-                        <label>
-                            <input
-                                type="file"
-                                onChange={(e) => this.setState({ selectedFile: e.target.files[0] })} //only accepts 1 fileupload
-                            />
-                        </label>
-                    </div>
+                <div>
+                    <label>
+                        <input
+                            type="file"
+                            onChange={(e) => setFormData({...formData, selectedFile: e.target.files[0]})} //only accepts 1 fileupload
+                        />
+                    </label>
+                </div>
 
-                    <button type="button" onClick={this.handleSubmit}>Send Sikker Melding</button>
-                </form>
-            </div>
-        )
-    }
+                <button type="button" onClick={handleSubmit}>Send Sikker Melding</button>
+            </form>
+        </div>
+    )
 }
 
 export default Form
